fa929a2241f14cae39c56dfc54b976ffaf628464
Adekunle Okunade <okunadeadekunle244@gmail.com>
2022-12-08 05:44:57 -0800

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,22 +4,20 @@
 
 /**
  *
- *  * main - Entry point
+ *  * main - main for printf
  *
- *   *
+ *   * Return: 0
  *
- *    * Return: Always 0 (success)
- *
- *     */
+ *    */
 
 int main(void)
 
 {
 
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+		printf("with proper grammar, but the outcome is a piece of art,");
 
-			return (0);
+			printf("with proper grammar, but the outcome is a piece of art,\n");
 
-
+				return (0);
 
 }
