97beb31a6a4727c886eb201122a9262ba7c82f33
Adekunle Okunade <okunadeadekunle244@gmail.com>
2022-12-08 07:22:45 -0800

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,27 +1,19 @@
-#include <stdio.h>
-
 #include <unistd.h>
 
 
 
 /**
+ * main - Prints a text
  *
- *  * main - prints exactly and that piece of art is useful
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * Return: Always 1 (Success)
+ */
 
 int main(void)
 
 {
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-			return (1);
+	return (1);
 
-
-
-}
+}     
