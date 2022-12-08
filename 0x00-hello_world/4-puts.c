e52b573a5469f207a0fcd636137e44f39093a9b2
Adekunle Okunade <okunadeadekunle244@gmail.com>
2022-12-08 05:34:48 -0800

    4task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
-
-
-/**
+/*
  *
  *  * main - Entry point
  *
@@ -16,7 +14,7 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+		puts("Programming is like building a multilingual puzzle\n");
 
 			return (0);
 
