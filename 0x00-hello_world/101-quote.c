3120d5a6c8490dec49d8eb6aeb8a24ffd8edaf6d
Adekunle Okunade <okunadeadekunle244@gmail.com>
2022-12-08 07:35:06 -0800

    101-q

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,23 @@
+#include <stdio.h>
+
 #include <unistd.h>
 
-
 /**
- * main - Prints a text
- *
- * Return: Always 1 (Success)
- *
+ * main - Entry point
+ * Create variables
+ * Use write
+ * Use the sizeof Operator
+ * Return: 1
  */
+
 
 int main(void)
 
 {
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+
 	return (1);
 
-}   
+}
