8783c3cdada926170d1c2f3d6bb414ed1a48f5bf
Adekunle Okunade <okunadeadekunle244@gmail.com>
2022-12-19 07:47:10 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main -  takes a pointer to an int as parameter and updates the value to 98
+* By Adekunle Leo
+* Return: Always 0 (Success)
+*/
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+
+	return 0;
+}
+
